877690043402fda69bd5604f68313ba3bf10982a
Sidney Schultz <tewzdk@gmail.com>
2017-12-19 01:03:02 +0100

    19/12-2017 - second

--- src/Character.java
+++ src/Character.java
@@ -20,86 +20,7 @@ public class Character {
     private int loot;
     private int creature;
 
-
-    public int Angreb(int min, int max) {
-        boolean minbool = true;
-        int hit;
-        Random random = new Random();
-
-        hit = random.nextInt(max)+1;
-        while (minbool) {
-            if (hit < min) {
-                hit = random.nextInt(max)+1;
-            } else if (hit == min || hit > min){
-                minbool = false;
-            }
-        }
-        return hit;
-    }
-
-    public void gemHealth() {
-        try {
-            PrintWriter outputStream = new PrintWriter(new File("saves/health"));
-                outputStream.println(
-                  health + ";" + armor + ";" + equippedWeapon + ";" + gold + ";" + navn + ";" + loot + ";" + creature + ";"
-                );
-            outputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    public void laesHealth() {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new File("saves/health")).useDelimiter(";").useLocale(Locale.US);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        while (scanner.hasNextInt()) {
-            health = scanner.nextInt();
-            armor = scanner.nextInt();
-            equippedWeapon = scanner.nextInt();
-            gold = scanner.nextInt();
-            navn = scanner.next();
-            loot = scanner.nextInt();
-            creature = scanner.nextInt();
-            scanner.nextLine();
-        }
-    }
-
-    public void gemWeaponList() {
-        try {
-            PrintWriter outputStream = new PrintWriter(new File("saves/weapons"));
-            for (int i = 0; i < egedeVåben.size(); i++) {
-                outputStream.println(
-                        egedeVåben.get(i).intValue()+";");
-            }
-            outputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    public void laesWeaponList() {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new File("saves/weapons")).useDelimiter(";").useLocale(Locale.US);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        while (scanner.hasNextInt()) {
-            int våben = scanner.nextInt();
-
-            egedeVåben.add(våben);
-            scanner.nextLine();
-        }
-    }
-
+    //WEAPONS
     public static Weapon weapons(int svar) {
         Weapon weapon = new Weapon("",0,0);
         switch (svar) {
@@ -138,59 +59,130 @@ public class Character {
         }
         return weapon;
     }
+    //ANGREB
+    public int Angreb(int min, int max) {
+        boolean minbool = true;
+        int hit;
+        Random random = new Random();
 
+        hit = random.nextInt(max)+1;
+        while (minbool) {
+            if (hit < min) {
+                hit = random.nextInt(max)+1;
+            } else if (hit == min || hit > min){
+                minbool = false;
+            }
+        }
+        return hit;
+    }
+    //GEMHEALTH
+    public void gemHealth() {
+        try {
+            PrintWriter outputStream = new PrintWriter(new File("saves/health"));
+                outputStream.println(
+                  health + ";" + armor + ";" + equippedWeapon + ";" + gold + ";" + navn + ";" + loot + ";" + creature + ";"
+                );
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public void laesHealth() {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("saves/health")).useDelimiter(";").useLocale(Locale.US);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNextInt()) {
+            health = scanner.nextInt();
+            armor = scanner.nextInt();
+            equippedWeapon = scanner.nextInt();
+            gold = scanner.nextInt();
+            navn = scanner.next();
+            loot = scanner.nextInt();
+            creature = scanner.nextInt();
+            scanner.nextLine();
+        }
+    }
+    //GEMWEAPONLIST
+    public void gemWeaponList() {
+        try {
+            PrintWriter outputStream = new PrintWriter(new File("saves/weapons"));
+            for (int i = 0; i < egedeVåben.size(); i++) {
+                outputStream.println(
+                        egedeVåben.get(i).intValue()+";");
+            }
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public void laesWeaponList() {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("saves/weapons")).useDelimiter(";").useLocale(Locale.US);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNextInt()) {
+            int våben = scanner.nextInt();
+
+            egedeVåben.add(våben);
+            scanner.nextLine();
+        }
+    }
+    //HEALTH
     public int getHealth() {
         return health;
     }
-
-    public String getNavn() {
-        return navn;
-    }
-
-    public void setNavn(String navn) {
-        this.navn = navn;
-    }
-
     public void setHealth(int health) {
         this.health = health;
     }
-
+    //NAVN
+    public String getNavn() {
+        return navn;
+    }
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+    //ARMOR
     public int getArmor() {
         return armor;
     }
-
     public void setArmor(int armor) {
         this.armor = armor;
     }
-
+    //EQUIPPEDWEAPON
     public int getEquippedWeapon() {
         return equippedWeapon;
     }
-
     public void setEquippedWeapon(int equippedWeapon) {
         this.equippedWeapon = equippedWeapon;
     }
-
+    //GOLD
     public int getGold() {
         return gold;
     }
-
     public void setGold(int gold) {
         this.gold = gold;
     }
 
+    //LOOT
     public int getLoot() {
         return loot;
     }
-
     public void setLoot(int loot) {
         this.loot = loot;
     }
-
+    //CREATURE
     public int getCreature() {
         return creature;
     }
-
     public void setCreature(int creature) {
         this.creature = creature;
     }
